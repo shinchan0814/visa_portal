d1ac496a7bb77bc2220651aebe92e63ce6cd2c76
Mihir Mahesh Patil <patilmihir352001@gmail.com>
2024-05-15 16:07:08 +0530

    Changes done

--- src/CountryDetailPage.js
+++ src/CountryDetailPage.js
@@ -108,7 +108,7 @@ const CountryDetailPage = () => {
                 {/* Mobile Header with Hamburger Menu */}
                 <Box
                     sx={{
-                        width: ['60%', '50%'],
+                        width: ['55%', '50%'],
                         height: 50,
                         display: { xs: 'flex', sm: 'none' }, // Show flex display on small screens, hide on larger screens
                         alignItems: 'center',
