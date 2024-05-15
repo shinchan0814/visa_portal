31342e5b9e4bef3a2cc4a727f5e316632e7014d1
Mihir Mahesh Patil <patilmihir352001@gmail.com>
2024-05-16 00:50:44 +0530

    Height

--- src/FaqSection.js
+++ src/FaqSection.js
@@ -27,7 +27,7 @@ const FaqSection = ({ data = [] }) => {
 
   const divStyle = {
     width: isMobile ? '100%' : '360px',
-    height: isMobile ? '80vh' : '800px',
+    height: isMobile ? '100vh' : '800px',
     borderRadius: '10px',
     padding: isMobile ? '5px' : '10px',
     position: 'relative',
