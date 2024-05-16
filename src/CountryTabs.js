6a94b8e6f88aac880dc245eb66fa94d617a5e625
Mihir Mahesh Patil <patilmihir352001@gmail.com>
2024-05-16 14:30:43 +0530

    Chnages done

--- src/CountryTabs.js
+++ src/CountryTabs.js
@@ -19,7 +19,7 @@ const CountryTabs = ({ activeTab, setActiveTab }) => {
         <div
             style={{
                 marginTop: '30px',
-                width: isMobile ?'100%': '60%',
+                width: isMobile ?'110%': '60%',
                 display: 'flex',
                 justifyContent: 'space-between',
                 marginRight: isMobile ? '-10px' : '',
