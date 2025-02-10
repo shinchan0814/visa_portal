1a4118170de136d2257ac646e536daa946e52817
Nmait2111 <namitjain2111@gmail.com>
2025-02-10 20:46:42 +0530

    sc

--- src/lib/fpixel.js
+++ src/lib/fpixel.js
@@ -1,10 +1,15 @@
-export const FB_PIXEL_ID ="1121954586079830";
+export const FB_PIXEL_ID = "1121954586079830";
 
-export const pageview = () => {
-  window.fbq("track", "PageView");
+// Track standard page views
+export const pageview = (url) => {
+  window.fbq("track", "PageView", { path: url });
 };
 
-// https://developers.facebook.com/docs/facebook-pixel/advanced/
+// Track standard & custom events correctly
 export const event = (name, options = {}) => {
-  window.fbq("track", name, options);
+  if (["PageView", "Purchase", "Lead", "CompleteRegistration"].includes(name)) {
+    window.fbq("track", name, options); // Use "track" for standard events
+  } else {
+    window.fbq("trackCustom", name, options); // Use "trackCustom" for non-standard events
+  }
 };
