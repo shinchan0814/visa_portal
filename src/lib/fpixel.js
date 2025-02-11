18979ed9541734e1fb8a5d720c1680038b2712b8
Nmait2111 <namitjain2111@gmail.com>
2025-02-11 10:18:16 +0530

    GTM added

--- src/lib/fpixel.js
+++ src/lib/fpixel.js
@@ -3,9 +3,11 @@ export const FB_PIXEL_ID = "1121954586079830";
 // Track standard page views
 export const pageview = (url) => {
   window.fbq("track", "PageView", { path: url });
+  console.log("pageview");
 };
 export const slugview = (url) =>{
   window.fbq('trackCustom',"slugView",{path:"abc"})
+  console.log("slugview");
 }
 
 // Track standard & custom events correctly
@@ -14,5 +16,6 @@ export const event = (name, options = {}) => {
     window.fbq("track", name, options); // Use "track" for standard events
   } else {
     window.fbq("trackCustom", name, options); // Use "trackCustom" for non-standard events
+    console.log("event");
   }
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -31,6 +31,24 @@ function App({ Component, pageProps }) {
   // return <Component {...pageProps} />;
   return (
     <>
+
+     {/* Google Tag Manager - Head */}
+     <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TVKSSM2W');
+          `,
+        }}
+      />
+
+
+
      <Script
         id="fb-pixel"
         strategy="afterInteractive"
@@ -48,6 +66,17 @@ function App({ Component, pageProps }) {
           `,
         }}
       />
+
+
+      {/* Google Tag Manager - Body */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-TVKSSM2W"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
 <Component {...pageProps} />
     </>
   );
